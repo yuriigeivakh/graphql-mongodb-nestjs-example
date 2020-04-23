10b58c41bf04686ef54e3297cf72efe5a4eee64f
yuriigeivakh <yurageyvakh@gmail.com>
2020-04-23 16:35:55 +0300

    Add validation: create lesson input

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { v4 as uuid } from 'uuid';
 import { Lesson } from './lesson.entity';
 import { Repository } from 'typeorm';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -11,7 +12,8 @@ export class LessonService {
         private lessonRepository: Repository<Lesson>,
     ) {}
 
-    async createLesson(name, startDate, endDate): Promise<Lesson> {
+    async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+        const { name, startDate, endDate } = createLessonInput;
         const lesson = this.lessonRepository.create({
             id: uuid(),
             name,
